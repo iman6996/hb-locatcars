b2f7900b6688680458846057870955d354002de2
Iman <ipernin@humanbooster.com>
2020-02-11 10:15:29 +0100

    first template -View

--- src/controller/CarsController.php
+++ src/controller/CarsController.php
@@ -5,12 +5,21 @@ namespace App\Controller;
 class CarsController{
 
 public function index(){
+$cars =[ 
+    [
+        "brand" =>"Ford",
+        "model" =>"Fiesta",
+    ],
+    [
+        "brand"=>"Citroën",
+        "model"=>"C3",
+    ],
+];
+
+
+
     include __DIR__ .'/../../template/cars/index.php';
 }
-
-
-
-
 }
 
 
--- template/cars/index.php
+++ template/cars/index.php
@@ -13,6 +13,11 @@
 
 <body>
 <h1>Les Carrrrrrrrrrrrrrrrrs</h1>
+<ul>
+<?php foreach($cars as $car ): ?>
+<li><?=$car["brand"]?><?php echo $car["model"]?></li>
+<?php endforeach ?>
+</ul>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
